e67e07655a6f952dbff7bf17a4a477d48dec608e
mpeteffi <murillo.peteffi@hotmail.com>
2017-05-31 15:43:22 -0300

    feat(Tickets): adicionar estrutura básica do CRUD de tickets.

---
+++ app/src/main/java/br/com/cwi/moses/TicketListActivity.java
@@ -0,0 +1,35 @@
+package br.com.cwi.moses;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TicketListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_list);
+
+        setTitle("Tickets");
+    }
+
+    public void novoTicketSugestao(View view) {
+        this.goToNewTicket("Sugestão");
+    }
+
+    public void novoTicketDuvida(View view) {
+        this.goToNewTicket("Dúvida");
+    }
+
+    public void novoTicketReclamacao(View view) {
+        this.goToNewTicket("Problema");
+    }
+
+    private void goToNewTicket(String ticketTipo) {
+        Intent intentTicketForm = new Intent(this, TicketFormActivity.class);
+        intentTicketForm.putExtra("TICKET_TIPO", ticketTipo);
+        startActivity(intentTicketForm);
+    }
+}
