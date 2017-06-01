f24f3a43450d394f003164990cef14384fb9f04b
mpeteffi <murillo.peteffi@hotmail.com>
2017-06-01 17:41:17 -0300

    feat(TicketDetalhe): adicionar tela de detalhe e função de clique na lista.

---
+++ app/src/main/java/br/com/cwi/moses/activities/TicketDetalheActivity.java
@@ -0,0 +1,15 @@
+package br.com.cwi.moses.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.cwi.moses.R;
+
+public class TicketDetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_detalhe);
+    }
+}
